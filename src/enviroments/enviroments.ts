1884daa59a99c5e0781658e3bbff23429bc31764
Eduard Farinyes <efarinyes@gmail.com>
2023-10-14 20:08:46 +0200

    Petites modificacions

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -1,5 +1,6 @@
 
 
 export const enviroments = {
-  baseUrl: 'http://localhost:3000'
+  // baseUrl: 'http://localhost:3000' URL de desenvolupament
+  baseUrl:'https://backend-nest.up.railway.app'
 }
